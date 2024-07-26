b53806825e33f7c2d0dc43d602446bd584dcfe0f
Maxim Poverenov <poverenov.92@gmail.com>
2024-07-26 17:55:10 +0200

    add CBV

--- catalog/management/settings.py
+++ catalog/management/settings.py
@@ -1,23 +1,12 @@
-
 from pathlib import Path
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+SECRET_KEY = 'django-insecure-6!9235%+#kak3r$$!6vv1v%66j6i!7or^+(#6f56=mw0&@tvr0'
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure--vh&=#q8p8+a!y01%eq^0(@2t(hdf#^8cq7d@*i4m8&ua&8f5e'
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
-
-
-# Application definition
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -26,6 +15,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'catalog',
     'blog',
 ]
@@ -76,7 +66,6 @@ DATABASES = {
 }
 
 
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
@@ -95,18 +84,13 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
-# Internationalization
-# https://docs.djangoproject.com/en/5.0/topics/i18n/
-
-#LANGUAGE_CODE = 'en-us'
-
-#TIME_ZONE = 'UTC'
-
-#USE_I18N = True
-
-#USE_TZ = True
-
+# LANGUAGE_CODE = 'en-us'
+#
+# TIME_ZONE = 'UTC'
+#
+# USE_I18N = True
+#
+# USE_TZ = True
 LANGUAGE_CODE = 'ru-ru'
 TIME_ZONE = 'Europe/Moscow'
 USE_I18N = True
